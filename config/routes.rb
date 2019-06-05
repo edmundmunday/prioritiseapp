c98b295996f42f9491a31d4cce2549556a71c47d
edmund munday <edmund.munday@gmail.com>
2019-06-05 02:59:58 +0000

    added navbar and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+root 'features#index'
+  devise_for :users
+  devise_for :admins
   resources :features
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
