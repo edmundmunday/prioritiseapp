c80d7c88731be1aa9d38f8629f9f6cfa793fdd05
edmund munday <edmund.munday@gmail.com>
2019-06-05 06:39:57 +0000

    closing off for day 1

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,3 +1,6 @@
 class Priority < ApplicationRecord
     belongs_to :feature
+    validates :new_sales, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+    validates :existing_customers, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+    validates :strategic, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 end
